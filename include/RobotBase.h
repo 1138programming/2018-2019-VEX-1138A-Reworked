3bdace329e73fb50f80f32c315082d98186e1790
ciherrera20 <ciherrera20@chaminet.org>
2018-07-18 13:07:06 -0700

    Updated library

--- include/RobotBase.h
+++ include/RobotBase.h
@@ -18,18 +18,53 @@ class RobotBase {
         robotInstance->doOneCycle();
       }
     };
-  protected:
-    virtual void robotInit();
-    virtual void autonInit();
-    virtual void autonPeriodic();
-    virtual void teleopInit();
-    virtual void teleopPeriodic();
-    virtual void disabledInit();
-    virtual void disabledPeriodic();
-  public:
     void doOneCycle();
+  protected:
+    /**
+      * This runs once when the robot starts up.
+      */
+    virtual void robotInit();
+
+    /**
+      * This runs once each time the autonomous period begins.
+      */
+    virtual void autonInit();
+
+    /**
+      * This runs in a loop during the autonomous period.
+      */
+    virtual void autonPeriodic();
+
+    /**
+      * This runs once each time the teleoperated period begins.
+      */
+    virtual void teleopInit();
+
+    /**
+      * This runs in a loop during the teleoperated period.
+      */
+    virtual void teleopPeriodic();
+
+    /**
+      * This runs once each time the robot is disabled.
+      */
+    virtual void disabledInit();
+
+    /**
+      * This runs in a loop whenever the robot is disabled.
+      */
+    virtual void disabledPeriodic();
+
+  public:
     RobotBase();
 
+    /**
+      * Run the robot.
+      *
+      * This should be called with the main robot class as the template argument.
+      * For example, if your robot is named `ExampleRobot`, you would call
+      * it with `RobotBase::runRobot<ExampleRobot>()`
+      */
     template <class RobotMain> static void runRobot() {
       // Just saying, if this doesn't work, try using the reinterepret cast on the method instead, instead of its pointer
       taskCreate(reinterpret_cast<void (*)(void*)>(&_privateRunRobot<RobotMain>), TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
--- include/subsystems/Subsystem.h
+++ include/subsystems/Subsystem.h
@@ -2,6 +2,7 @@
 #define _SUBSYSTEMS_SUBSYSTEM_H_
 
 #include "main.h"
+#include "commands/Command.h"
 
 class Command;
 
@@ -9,9 +10,23 @@ class Subsystem {
   private:
     Command* defaultCommand = NULL;
   protected:
+    /**
+      * Set the default command for this subsystem. Should only be called in initDefaultCommand()
+      *
+      * @param aCommand a Command* to the new default command
+      */
     void setDefaultCommand(Command* aCommand);
   public:
+    /**
+      * Initializes the default command. This should be the only place setDefaultCommand() is called.
+      */
     virtual void initDefaultCommand();
+
+    /**
+      * Get this subsystem's default command.
+      *
+      * @return This subsystem's default command
+      */
     Command* getDefaultCommand();
     Command* currentCommand = NULL;
     Subsystem();
